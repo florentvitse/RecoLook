cd111833cde27c0b2baa28d5bf8963a01656d842
Florent <florentlepro@hotmail.fr>
2015-09-08 12:42:32 +0200

    Add reference to OpenCV library

--- gen/com/example/recolouke/R.java
+++ gen/com/example/recolouke/R.java
@@ -9,6 +9,32 @@ package com.example.recolouke;
 
 public final class R {
     public static final class attr {
+        /** <p>May be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+<p>May be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>any</code></td><td>-1</td><td></td></tr>
+<tr><td><code>back</code></td><td>99</td><td></td></tr>
+<tr><td><code>front</code></td><td>98</td><td></td></tr>
+</table>
+         */
+        public static int camera_id=0x7f010001;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static int show_fps=0x7f010000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -18,73 +44,76 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static int activity_horizontal_margin=0x7f040000;
+        public static int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int amd=0x7f020000;
-        public static final int auchan=0x7f020001;
-        public static final int bmw=0x7f020002;
-        public static final int carrefour=0x7f020003;
-        public static final int disney=0x7f020004;
-        public static final int e_leclerc=0x7f020005;
-        public static final int ea_sports=0x7f020006;
-        public static final int edf=0x7f020007;
-        public static final int fedex=0x7f020008;
-        public static final int fff=0x7f020009;
-        public static final int ic_launcher=0x7f02000a;
-        public static final int kfc=0x7f02000b;
-        public static final int lidl=0x7f02000c;
-        public static final int nike=0x7f02000d;
-        public static final int orange=0x7f02000e;
-        public static final int progress=0x7f02000f;
-        public static final int progress_background=0x7f020010;
-        public static final int starbucks=0x7f020011;
-        public static final int telecom_lille=0x7f020012;
+        public static int amd=0x7f020000;
+        public static int auchan=0x7f020001;
+        public static int bmw=0x7f020002;
+        public static int carrefour=0x7f020003;
+        public static int disney=0x7f020004;
+        public static int e_leclerc=0x7f020005;
+        public static int ea_sports=0x7f020006;
+        public static int edf=0x7f020007;
+        public static int fedex=0x7f020008;
+        public static int fff=0x7f020009;
+        public static int ic_launcher=0x7f02000a;
+        public static int kfc=0x7f02000b;
+        public static int lidl=0x7f02000c;
+        public static int nike=0x7f02000d;
+        public static int orange=0x7f02000e;
+        public static int progress=0x7f02000f;
+        public static int progress_background=0x7f020010;
+        public static int starbucks=0x7f020011;
+        public static int telecom_lille=0x7f020012;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int brand=0x7f08000d;
-        public static final int btnAnalyse=0x7f08000a;
-        public static final int btnGallery=0x7f080006;
-        public static final int btnPhoto=0x7f080007;
-        public static final int btnReturn=0x7f080000;
-        public static final int btnReturnShowAnalyse=0x7f08000b;
-        public static final int btnTestNext=0x7f080002;
-        public static final int imgToAnalyse=0x7f080001;
-        public static final int imgView=0x7f080009;
-        public static final int layoutContainer=0x7f080004;
-        public static final int layoutImage=0x7f080008;
-        public static final int layoutPhoto=0x7f080005;
-        public static final int listview_widget=0x7f080003;
-        public static final int logo=0x7f08000e;
-        public static final int percentageBarComparison=0x7f08000c;
+        public static int action_settings=0x7f080012;
+        public static int any=0x7f080000;
+        public static int back=0x7f080001;
+        public static int brand=0x7f080010;
+        public static int btnAnalyse=0x7f08000d;
+        public static int btnGallery=0x7f080009;
+        public static int btnPhoto=0x7f08000a;
+        public static int btnReturn=0x7f080003;
+        public static int btnReturnShowAnalyse=0x7f08000e;
+        public static int btnTestNext=0x7f080005;
+        public static int front=0x7f080002;
+        public static int imgToAnalyse=0x7f080004;
+        public static int imgView=0x7f08000c;
+        public static int layoutContainer=0x7f080007;
+        public static int layoutImage=0x7f08000b;
+        public static int layoutPhoto=0x7f080008;
+        public static int listview_widget=0x7f080006;
+        public static int logo=0x7f080011;
+        public static int percentageBarComparison=0x7f08000f;
     }
     public static final class layout {
-        public static final int activity_analyse=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_show_analyse=0x7f030002;
-        public static final int listview=0x7f030003;
+        public static int activity_analyse=0x7f030000;
+        public static int activity_main=0x7f030001;
+        public static int activity_show_analyse=0x7f030002;
+        public static int listview=0x7f030003;
     }
     public static final class menu {
-        public static final int analyse=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int show_analyse=0x7f070002;
+        public static int analyse=0x7f070000;
+        public static int main=0x7f070001;
+        public static int show_analyse=0x7f070002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int btnAnalyse=0x7f050004;
-        public static final int btnGallery=0x7f050002;
-        public static final int btnPhoto=0x7f050003;
-        public static final int btnReturn=0x7f050007;
-        public static final int contentImgView=0x7f050005;
-        public static final int hello_world=0x7f050009;
-        public static final int title_activity_analyse=0x7f050006;
-        public static final int title_activity_show_analyse=0x7f050008;
+        public static int action_settings=0x7f050001;
+        public static int app_name=0x7f050000;
+        public static int btnAnalyse=0x7f050004;
+        public static int btnGallery=0x7f050002;
+        public static int btnPhoto=0x7f050003;
+        public static int btnReturn=0x7f050007;
+        public static int contentImgView=0x7f050005;
+        public static int hello_world=0x7f050009;
+        public static int title_activity_analyse=0x7f050006;
+        public static int title_activity_show_analyse=0x7f050008;
     }
     public static final class strings {
-        public static final int contentImgView=0x7f090000;
+        public static int contentImgView=0x7f090000;
     }
     public static final class style {
         /** 
@@ -108,10 +137,65 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static int AppTheme=0x7f060001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CameraBridgeViewBase.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CameraBridgeViewBase_camera_id com.example.recolouke:camera_id}</code></td><td></td></tr>
+           <tr><td><code>{@link #CameraBridgeViewBase_show_fps com.example.recolouke:show_fps}</code></td><td></td></tr>
+           </table>
+           @see #CameraBridgeViewBase_camera_id
+           @see #CameraBridgeViewBase_show_fps
+         */
+        public static final int[] CameraBridgeViewBase = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.example.recolouke.R.attr#camera_id}
+          attribute's value can be found in the {@link #CameraBridgeViewBase} array.
+
+
+          <p>May be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+<p>May be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>any</code></td><td>-1</td><td></td></tr>
+<tr><td><code>back</code></td><td>99</td><td></td></tr>
+<tr><td><code>front</code></td><td>98</td><td></td></tr>
+</table>
+          @attr name com.example.recolouke:camera_id
+        */
+        public static int CameraBridgeViewBase_camera_id = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.example.recolouke.R.attr#show_fps}
+          attribute's value can be found in the {@link #CameraBridgeViewBase} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.recolouke:show_fps
+        */
+        public static int CameraBridgeViewBase_show_fps = 0;
+    };
 }
